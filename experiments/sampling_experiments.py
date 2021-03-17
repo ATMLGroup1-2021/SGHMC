318676e21c73283f53d89e4c7b62e0dc806f68c5
Ipshita <chatterjeei08@gmail.com>
2021-03-17 11:11:44 +0000

    Modify bin size and regenerate basic and builtin hmc sampling plots for smoother curves

--- experiments/sampling_experiments.py
+++ experiments/sampling_experiments.py
@@ -1,11 +1,16 @@
-import os, sys, torch
+import os
+import sys
 import matplotlib.pyplot as plt
 import numpy as np
+import distributions
 from pyro.infer.mcmc import MCMC
 from pyro.infer.mcmc.nuts import HMC
+
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
 from samplers.basic_hmc import BasicHMC
-from samplers.sghmc import SGHMC
-import distributions
+
 
 '''
 Perform sampling experiments for built-in Pyro HMC 
@@ -21,10 +26,6 @@ python sampling_experiments.py basic
 
 Result will be saved in figures/sampling_experiments
 '''
-
-currentdir = os.path.dirname(os.path.realpath(__file__))
-parentdir = os.path.dirname(currentdir)
-sys.path.append(parentdir)
 
 # Define HMC kernel based on argument
 kernel = None
@@ -49,15 +50,17 @@ def hmc_sampling(dist):
 
 # Code for generating plots for various distributions
 
+BIN_SIZE = 100
+
 # Unimodal distributions
 fig, ax1 = plt.subplots(1, 2, figsize=(16, 4))
 
 unimodal_sym_posterior = hmc_sampling(distributions.unimodal_sym)
-ax1[0].hist(unimodal_sym_posterior['sample'].numpy(), rwidth=0.3)
+ax1[0].hist(unimodal_sym_posterior['sample'].numpy(), bins=BIN_SIZE)
 ax1[0].set_title('Unimodal symmetric distribution with ' + option + ' HMC sampler')
 
 unimodal_asym_posterior = hmc_sampling(distributions.unimodal_asym)
-ax1[1].hist(unimodal_asym_posterior['sample'].numpy(), rwidth=0.3)
+ax1[1].hist(unimodal_asym_posterior['sample'].numpy(), bins=BIN_SIZE)
 ax1[1].set_title('Unimodal asymmetric distribution with ' + option + ' HMC sampler')
 plt.savefig("figures/sampling_experiments/unimodal_" + option + ".jpg")
 
@@ -69,7 +72,7 @@ sample1, sample2, weight = bimodal_symmetric_posterior["sample1"], bimodal_symme
                            bimodal_symmetric_posterior["weight"]
 bimodal_symmetric_posterior = np.array([[sample1[i], sample2[i]][weight[i] < 0.5] for i in range(len(sample1))])
 
-ax2[0].hist(bimodal_symmetric_posterior, rwidth=0.3)
+ax2[0].hist(bimodal_symmetric_posterior, bins=BIN_SIZE)
 ax2[0].set_title('Bimodal symmetric distribution with ' + option + ' HMC sampler')
 
 bimodal_asymmetric_posterior = hmc_sampling(distributions.bimodal_asym)
@@ -77,7 +80,7 @@ sample1, sample2, weight = bimodal_asymmetric_posterior["sample1"], bimodal_asym
                            bimodal_asymmetric_posterior["weight"]
 bimodal_asymmetric_posterior = np.array([[sample1[i], sample2[i]][weight[i] < 0.5] for i in range(len(sample1))])
 
-ax2[1].hist(bimodal_asymmetric_posterior, rwidth=0.3)
+ax2[1].hist(bimodal_asymmetric_posterior, bins=BIN_SIZE)
 ax2[1].set_title('Bimodal asymmetric distribution with ' + option + ' HMC sampler')
 plt.savefig("figures/sampling_experiments/bimodal_" + option + ".jpg")
 
@@ -91,7 +94,7 @@ trimodal_symmetric_posterior = np.array(
     [[sample1[i], sample2[i], sample3[i]][0 if weight[i] < 1 / 3 else (2 if weight[i] > 2 / 3 else 1)] for i in
      range(len(sample1))])
 
-ax3[0].hist(trimodal_symmetric_posterior, rwidth=0.3)
+ax3[0].hist(trimodal_symmetric_posterior, bins=BIN_SIZE)
 ax3[0].set_title('Trimodal symmetric distribution with ' + option + ' HMC sampler')
 
 trimodal_asymmetric_posterior = hmc_sampling(distributions.trimodal_asym)
@@ -101,6 +104,6 @@ trimodal_asymmetric_posterior = np.array(
     [[sample1[i], sample2[i], sample3[i]][0 if weight[i] < 1 / 3 else (2 if weight[i] > 2 / 3 else 1)] for i in
      range(len(sample1))])
 
-ax3[1].hist(trimodal_asymmetric_posterior, rwidth=0.3)
+ax3[1].hist(trimodal_asymmetric_posterior, bins=BIN_SIZE)
 ax3[1].set_title('Trimodal asymmetric distribution with ' + option + ' HMC sampler')
 plt.savefig("figures/sampling_experiments/trimodal_" + option + ".jpg")
