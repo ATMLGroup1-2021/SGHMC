21fd48db6fac7652e1aee73a248f3c1d53d96616
Charlie <charlielondon02@gmail.com>
2021-03-17 12:56:28 +0000

    beta binomial and gamma poisson

--- experiments/analytic_posteriors.py
+++ experiments/analytic_posteriors.py
@@ -6,15 +6,14 @@ from functools import partial
 from torch import matmul
 
 
-class _2dGaussianDataset(Dataset):
-    def __init__(self, mean, covar, size):
-        d = dist.MultivariateNormal(mean, covariance_matrix=covar)
-        self.size = size
-        self.dataset = d.sample((size,))
+class DistributionDataset(Dataset):
+    def __init__(self, dist, num_samples):
+        self.num_samples = num_samples
+        self.dataset = dist.sample((num_samples,))
         super().__init__()
 
     def __len__(self):
-        return self.size
+        return self.num_samples
 
     def __getitem__(self, index):
         return self.dataset[index]
@@ -29,11 +28,12 @@ def _2d_gaussian_model(data, likelihood_covar):
 
 def _2d_gaussian(likelihood_mean, likelihood_covar, num_samples):
     '''
-    Takes in two (real) tensors, mean (2x1) and covar (2x2), creates dataset
-    Returns model, dataset, and analytic posterior
+        Takes in two (real) tensors, mean (2x1) and covar (2x2), and num_samples, and creates dataset
+        Returns model, dataset, and analytic posterior
     '''
 
-    dataset = _2dGaussianDataset(likelihood_mean, likelihood_covar, num_samples)
+    dataset = DistributionDataset(dist.MultivariateNormal(likelihood_mean, covariance_matrix=likelihood_covar),
+                                 num_samples)
     model = partial(_2d_gaussian_model, likelihood_covar)
 
     prior_mean = torch.zeros(2)
@@ -49,3 +49,44 @@ def _2d_gaussian(likelihood_mean, likelihood_covar, num_samples):
                             num_samples * matmul(inv_likelihood_covar, sample_mean))
 
     return dataset, model, dist.MultivariateNormal(posterior_mean, covariance_matrix=posterior_covar)
+
+
+def beta_bernoulli_model(data):
+    p = pyro.sample('p', dist.Beta(1, 1))
+    y = pyro.sample('y', dist.Bernoulli(p), obs=data)
+    return y
+
+
+def beta_bernoulli(p, num_samples):
+    '''
+        Takes in real p and num_samples, and creates dataset of num_samples draws from Bernoulli(p)
+        Returns model, dataset, and analytic posterior
+    '''
+
+    dataset = DistributionDataset(dist.Bernoulli(p), num_samples)
+    model = beta_bernoulli_model
+
+    successes = (dataset.dataset == torch.ones(num_samples)).sum().item()
+
+    return dataset, model, dist.Beta(1 + successes, 1 + num_samples - successes)
+
+
+def gamma_possion_model(data):
+    rate = pyro.sample('p', dist.Gamma(0.001, 0.001))
+    y = pyro.sample('y', dist.Poisson(rate), obs=data)
+    return y
+
+
+def gamma_possion(rate, num_samples):
+    '''
+        Takes in real p and num_samples, and creates dataset of num_samples draws from Bernoulli(p)
+        Returns model, dataset, and analytic posterior
+    '''
+
+    dataset = DistributionDataset(dist.Poisson(rate), num_samples)
+    model = gamma_possion_model
+
+    occurences = dataset.dataset.sum().item()
+    print(occurences)
+
+    return dataset, model, dist.Poisson(0.001 + occurences, 0.001 + num_samples)
