47591579adb4e2be95f1d32b5a98593f68d350f7
Charlie <charlielondon02@gmail.com>
2021-03-18 11:32:45 +0000

    scatter and trajectory plots

--- experiments/sghmc_experiments.py
+++ experiments/sghmc_experiments.py
@@ -8,7 +8,6 @@ from analytic_posteriors import beta_bernoulli, _2d_gaussian, gamma_poisson
 import torch
 from torch.utils.data import DataLoader
 from pyro.infer import MCMC
-import plotly.express as px
 import plotly.graph_objects as go
 currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
@@ -60,37 +59,123 @@ def gamma_poisson_sghmc(rate, num_samples, step_size=0.1):
     # posterior is over rate
     dataset, model, posterior = gamma_poisson(rate, num_samples)
     sghmc_samples = calculate_sghmc(dataset, model, step_size=step_size)['rate']
-    hmc_samples = calculate_hmc(dataset, model, step_size=0.005)['rate']
+    hmc_samples = calculate_hmc(dataset, model, step_size=step_size)['rate']
 
     return sghmc_samples, hmc_samples, posterior
 
 
-def _2d_plot(sghmc_samples, hmc_samples, analytical_posterior):
-    # fig = px.density_contour(x=samples[:, 0].tolist(), y=samples[:, 1].tolist())
-    # fig.show()
-    # plt.hist2d(samples[:, 0].tolist(), samples[:, 1].tolist(), bins=[20, 20])
-    # plt.show()
+def _2d_scatter_plot(sghmc_samples, hmc_samples, analytical_posterior):
+    sghmc_plot = go.Scatter(
+        x=sghmc_samples[:, 0].tolist()[0::5],
+        y=sghmc_samples[:, 1].tolist()[0::5],
+        mode='markers',
+        marker=dict(color="darkturquoise"),
+    )
+
+    hmc_plot = go.Scatter(
+        x=hmc_samples[:, 0].tolist()[0::10],
+        y=hmc_samples[:, 1].tolist()[0::10],
+        mode='markers',
+        marker=dict(color="hotpink")
+    )
 
     x_mean = analytical_posterior.mean[0].item()
     y_mean = analytical_posterior.mean[1].item()
-    x_coords, y_coords = torch.meshgrid(torch.linspace(x_mean - 2, x_mean + 2, 100),
-                                        torch.linspace(y_mean - 2, y_mean + 2, 100))
+    xrange = torch.linspace(x_mean - 1, x_mean + 1, 100)
+    yrange = torch.linspace(y_mean - 1, y_mean + 1, 100)
+    x_coords, y_coords = torch.meshgrid(xrange, yrange)
     coords = torch.stack((x_coords, y_coords), dim=2)
 
-    max = analytical_posterior.log_prob(analytical_posterior.mean).exp().item()
+    max_analytical = analytical_posterior.log_prob(analytical_posterior.mean).exp().item()
     dist_plot = analytical_posterior.log_prob(coords).exp()
 
-    sghmc_plot = go.Histogram2dContour(
+    analytic_plot = go.Contour(
+        z=dist_plot,
+        x=xrange.tolist(),  # horizontal axis
+        y=yrange.tolist(),  # vertical axis
+        contours_coloring='lines',
+        contours=dict(
+            start=0.1,
+            end=max_analytical,
+            size=0.1*max_analytical,
+        )
+    )
+
+    fig = go.Figure(data=(analytic_plot, sghmc_plot, hmc_plot))
+    fig.show()
+
+
+def _2d_trajectory_plot(samples, analytical_posterior):
+    hmc_plot = go.Scatter(
+        x=samples[:, 0].tolist()[-50::],
+        y=samples[:, 1].tolist()[-50::],
+        mode='lines+markers',
+        marker=dict(color="darkturquoise"),
+        line=dict(width=1, color="darkturquoise"),
+    )
+
+    x_mean = analytical_posterior.mean[0].item()
+    y_mean = analytical_posterior.mean[1].item()
+    xrange = torch.linspace(x_mean - 1, x_mean + 1, 100)
+    yrange = torch.linspace(y_mean - 1, y_mean + 1, 100)
+    x_coords, y_coords = torch.meshgrid(xrange, yrange)
+    coords = torch.stack((x_coords, y_coords), dim=2)
+
+    max_analytical = analytical_posterior.log_prob(analytical_posterior.mean).exp().item()
+    dist_plot = analytical_posterior.log_prob(coords).exp()
+
+    analytic_plot = go.Contour(
+        z=dist_plot,
+        x=xrange.tolist(),  # horizontal axis
+        y=yrange.tolist(),  # vertical axis
+        contours_coloring='lines',
+        contours=dict(
+            start=0.1,
+            end=max_analytical,
+            size=0.1 * max_analytical,
+        )
+    )
+
+    fig = go.Figure(data=(analytic_plot, hmc_plot))
+    fig.show()
+
+
+def _2d_plot(sghmc_samples, hmc_samples, analytical_posterior):
+    sghmc_bins, xedges, yedges = np.histogram2d(sghmc_samples[:, 0].tolist(), sghmc_samples[:, 1].tolist(),
+                                                bins=[20, 20])
+    xbins = dict(
+        start=xedges[0],
+        end=xedges[-1],
+        size=xedges[1]-xedges[0],
+    )
+    ybins = dict(
+        start=yedges[0],
+        end=yedges[-1],
+        size=yedges[1] - yedges[0],
+    )
+    xcenters = (xedges[:-1] + xedges[1:]) / 2
+    ycenters = (yedges[:-1] + yedges[1:]) / 2
+    max_sghmc = max(map(max, sghmc_bins))
+
+    sghmc_plot = go.Contour(
+        z=sghmc_bins,
+        x=xcenters,
+        y=ycenters,
+        contours_coloring="lines",
+        colorscale=[[0, "blue"], [1, "blue"]],
+        contours=dict(
+            start=0.1*max_sghmc,
+            end=max_sghmc,
+            size=0.1*max_sghmc,
+        ),
+    )
+
+    sghmc_plot2 = go.Histogram2dContour(
         x=sghmc_samples[:, 0].tolist(),
         y=sghmc_samples[:, 1].tolist(),
         contours_coloring="lines",
         colorscale=[[0, "blue"], [1, "blue"]],
-        # contours=dict(
-        #     start=0.1,
-        #     end=1,
-        #     size=0.1,
-        # ),
-        # histnorm="probability"
+        histnorm="probability"
     )
 
     hmc_plot = go.Histogram2dContour(
@@ -98,13 +183,17 @@ def _2d_plot(sghmc_samples, hmc_samples, analytical_posterior):
         y=hmc_samples[:, 1].tolist(),
         contours_coloring="lines",
         colorscale=[[0, "green"], [1, "green"]],
-        # contours=dict(
-        #     start=0.1,
-        #     end=1,
-        #     size=0.1,
-        # ),
-        # histnorm="probability"
+        histnorm="probability"
     )
+
+    x_mean = analytical_posterior.mean[0].item()
+    y_mean = analytical_posterior.mean[1].item()
+    x_coords, y_coords = torch.meshgrid(torch.linspace(x_mean - 2, x_mean + 2, 100),
+                                        torch.linspace(y_mean - 2, y_mean + 2, 100))
+    coords = torch.stack((x_coords, y_coords), dim=2)
+
+    max_analytical = analytical_posterior.log_prob(analytical_posterior.mean).exp().item()
+    dist_plot = analytical_posterior.log_prob(coords).exp()
 
     analytic_plot = go.Contour(
         z=dist_plot,
@@ -113,16 +202,14 @@ def _2d_plot(sghmc_samples, hmc_samples, analytical_posterior):
         contours_coloring='lines',
         colorscale=[[0, "red"], [1, "red"]],
         contours=dict(
-            start=0.1*max,
-            end=max,
-            size=0.1*max,
+            start=0.1*max_analytical,
+            end=max_analytical,
+            size=0.1*max_analytical,
         ),
     )
 
-    fig = go.Figure(data=(analytic_plot, hmc_plot, sghmc_plot))
+    fig = go.Figure(data=(analytic_plot, hmc_plot))
     fig.show()
-    # plt.contour(x_coords, y_coords, dist_plot)
-    # plt.show()
 
 
 def _1d_plot(sghmc_samples, hmc_samples, analytical_posterior):
@@ -149,11 +236,17 @@ def _1d_plot(sghmc_samples, hmc_samples, analytical_posterior):
 
 
 def main():
-    # _2d_plot(*(_2d_gauss_sghmc(torch.tensor([5., -8.]), torch.diag(torch.tensor([12., 6.])), 1000)))
+    gauss_sghmc, gauss_hmc, analytic_posterior = _2d_gauss_sghmc(torch.tensor([5., -8.]),
+                                                                 torch.diag(torch.tensor([12., 6.])), 1000,
+                                                                 step_size=0.05)
 
-    _1d_plot(*(beta_bernoulli_sghmc(0.7, 1000, step_size=0.05)))
+    # _2d_scatter_plot(gauss_sghmc, gauss_hmc, analytic_posterior)
+    _2d_trajectory_plot(gauss_sghmc, analytic_posterior)
+    _2d_trajectory_plot(gauss_hmc, analytic_posterior)
 
-    _1d_plot(*(gamma_poisson_sghmc(10, 1000, step_size=0.005)))
+    # _1d_plot(*(beta_bernoulli_sghmc(0.7, 1000, step_size=0.005)))
+    #
+    # _1d_plot(*(gamma_poisson_sghmc(10, 1000, step_size=0.005)))
 
 
 if __name__ == "__main__":
